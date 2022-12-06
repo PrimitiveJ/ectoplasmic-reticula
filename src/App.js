91d010523487ffdcac4681d881c612ca7f3c88c5
Primitive J <hokecj@gmail.com>
2022-12-05 21:05:50 -0500

    set up initial file structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Article, Brand, CTA, Feature, NavBar} from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
+
+const App = () => {
+  return (
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+  )
+}
+
+export default App
